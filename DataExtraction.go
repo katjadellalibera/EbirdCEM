191abd6d572744f98d59f5c03df6a1086616d16e
Katja Della Libera <40602167+katjadellalibera@users.noreply.github.com>
2020-01-25 19:05:50 -0300

    finished data output format, draft of export code

--- DataExtraction.go
+++ DataExtraction.go
@@ -7,38 +7,78 @@ import (
 	"io"
 	"math"
 	"os"
+	"path"
 	"strconv"
 )
 
 func main() {
+	dataDir := "C:\\Users\\katjad\\Desktop\\Random\\ebirdData\\eBirdDataGo"
 	// creating layers of Readers to extract the data from the compressed file without decompressing it
 	filepath := "C:\\Users\\katjad\\Desktop\\Random\\ebirdData\\ebird_reference_dataset_v2016_western_hemisphere.tar\\ebird_reference_dataset_v2016_western_hemisphere\\ERD2016SS\\2002\\checklists.csv.gz"
 	file, _ := os.Open(filepath)
 	gzipReader, _ := gzip.NewReader(file)
 	csvReader := csv.NewReader(gzipReader)
-	//
+	// read the first line of the file containing just the names
 	csvReader.Read()
-	counts := map[[2]float32]int{}
+	// initialize an empty map to hold a list of counts for total and species
+	counts := map[[2]float32][2]int{}
 
-	for i := 0; i < 1000; i++ {
+	i := 0
+	// run a for loop over the entire file
+	for {
+		//progress indicator after every 10000 checklists
+		i++
+		if i%10000 == 0 {
+			fmt.Println(i)
+		}
 		l, err := csvReader.Read()
+		// exit the loop when the end of file is reached
 		if err == io.EOF {
 			break
 		}
+		// only include primary checklists
 		if l[18] == "1" {
 			latfloat, errlat := strconv.ParseFloat(l[2], 32)
 			longfloat, errlong := strconv.ParseFloat(l[3], 32)
+			// skip locations with erroneous coordinates
 			if errlat != nil || errlong != nil {
 				continue
 			}
-			latitude := float32(math.Floor(latfloat*10) / 10)
-			longitude := float32(math.Floor(longfloat*10) / 10)
-			counts[[2]float32{latitude, longitude}] += 1
+			// round the latitude and longitude to the next 1-decimal to group locations in grid
+			latitude := float32(math.Floor(latfloat*1) / 1)
+			longitude := float32(math.Floor(longfloat*1) / 1)
+			location := [2]float32{latitude, longitude}
+			// add one to the total of observations in the square
+			temp := counts[location]
+			temp[0] += 1
+			// This adds the count for a particular bird to the total at the location
+			speciesint, errspecies := strconv.ParseInt(l[3915], 10, 32)
+			if errspecies != nil {
+				continue
+			}
+			if speciesint > 0 {
+				temp[1] += 1
+			}
+			counts[location] = temp
 		}
 	}
 
+	// check that the results are as expected
 	fmt.Println(counts)
 	fmt.Println(len(counts))
+
+	// Create a csv file to export to
+	resultFile, _ := os.Create(path.Join(dataDir, "red_breasted.csv"))
+	resultWriter := csv.NewWriter(resultFile)
+	// iterate through every location and write the coordinates and values
+	for location, value := range counts {
+		lat := strconv.FormatFloat(location[0], 'E', -1, 64)
+		long := strconv.FormatFloat(location[1], 'E', -1, 64)
+		resultWriter.Write([4]string{lat, long, strconv.Itoa(value[0]), strconv.Itoa(value[1])})
+	}
+	resultWriter.Flush()
+	resultFile.Close()
+
 	/*for i := 0; i < 1; i++ {
 		l, _ := csvReader.Read()
 		fmt.Println(l[0:20])
